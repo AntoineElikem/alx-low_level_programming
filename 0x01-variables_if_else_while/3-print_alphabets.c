2d5129d775e3466be36d6b1ef81d60399d635e0b
antoineelikem <antoinedzisam@yahoo.com>
2023-03-19 10:38:54 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Displays lowercase alphabet with putchar
+*
+* Return: 0 for success
+*/
+
+int main(void)
+{
+        int alphabet;
+	int capital;
+
+        alphabet = 'a';
+	capital = 'A';
+        while (alphabet <= 'z')
+        {
+                putchar(alphabet);
+                alphabet++;
+        }
+	while (capital <= 'Z')
+	{
+		putchar(capital);
+		capital++;
+	}
+        putchar('\n');
+        return (0);
+}
