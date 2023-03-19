98844b2f1f292fc2a70289ee72e30ba9c70b9ef1
antoineelikem <antoinedzisam@yahoo.com>
2023-03-19 11:36:49 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Display numbers from 0to9
+*
+* Return: 0 for success
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i=0; i<10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
